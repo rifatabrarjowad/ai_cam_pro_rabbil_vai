1186706a2a97543bfff18076880a8b2725a249e4
Rifat Abrar Jowad <rajdiprifat2004@gmail.com>
2021-04-16 00:52:27 +0600

    placeholder ready

--- my-app/src/Components/Camera.js
+++ my-app/src/Components/Camera.js
@@ -2,7 +2,7 @@ import React, {Component,Fragment} from 'react';
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import imagePlaceholder from '../Assets/image/imagePlaceholder.svg'
+import imagePlaceholder from '../Assets/image/imagePlaceholder.jpg'
 import Webcam from "react-webcam";
 
 
@@ -10,10 +10,19 @@ class Camera extends Component {
     constructor() {
         super();
         this.cameraRef=React.createRef();
+
+        this.state={
+            capturePhoto:imagePlaceholder,
+
+        }
+
     }
 
 
     onCapture=()=>{
+        let PhotoBas64 = this.cameraRef.current.getScreenshot();
+        this.setState({capturePhoto:PhotoBas64})
+
 
     }
 
@@ -29,13 +38,13 @@ class Camera extends Component {
                                 ref={this.cameraRef}
                                 audio={false}
                                 screenshotFormat="image/jpeg"
-
+                                style={{width:"500px"}}
 
                             />
                             <button onClick={this.onCapture} className="btn btn-primary">Capture</button>
                         </Col>
                         <Col className="p-2" md={6} sm={12} lg={6} >
-                            <img className='w-100 '  src={imagePlaceholder} />
+                            <img style={{width:"500px"}}  src={this.state.capturePhoto} />
                             <button className="btn btn-primary">Save</button>
                         </Col>
                     </Row>
