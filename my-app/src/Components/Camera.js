6594873d5b1c918096206cffa9c1ee912b16b9d6
Rifat Abrar Jowad <rajdiprifat2004@gmail.com>
2021-04-16 00:07:38 +0600

    placeholder ready

--- my-app/src/Components/Camera.js
+++ my-app/src/Components/Camera.js
@@ -3,19 +3,39 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import imagePlaceholder from '../Assets/image/imagePlaceholder.svg'
+import Webcam from "react-webcam";
+
 
 class Camera extends Component {
+    constructor() {
+        super();
+        this.cameraRef=React.createRef();
+    }
+
+
+    onCapture=()=>{
+
+    }
+
+
+
     render() {
         return (
             <Fragment>
                 <Container>
                     <Row className="mt-5 shadow-sm bg-white" >
                         <Col className="p-2" md={6} sm={12} lg={6} >
-                            <img className='w-100' src={imagePlaceholder} />
-                            <button className="btn btn-primary">Capture</button>
+                            <Webcam
+                                ref={this.cameraRef}
+                                audio={false}
+                                screenshotFormat="image/jpeg"
+
+
+                            />
+                            <button onClick={this.onCapture} className="btn btn-primary">Capture</button>
                         </Col>
                         <Col className="p-2" md={6} sm={12} lg={6} >
-                            <img className='w-100' src={imagePlaceholder} />
+                            <img className='w-100 '  src={imagePlaceholder} />
                             <button className="btn btn-primary">Save</button>
                         </Col>
                     </Row>
