fd1fb92b2db62e259b9038bd3cd4525e5d623246
Rifat Abrar Jowad <rajdiprifat2004@gmail.com>
2021-04-15 22:54:09 +0600

    placeholder ready

--- my-app/src/Components/Camera.js
+++ my-app/src/Components/Camera.js
@@ -1,11 +1,26 @@
-import React, {Component} from 'react';
+import React, {Component,Fragment} from 'react';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import imagePlaceholder from '../Assets/image/imagePlaceholder.svg'
 
 class Camera extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <Fragment>
+                <Container>
+                    <Row className="mt-5 shadow-sm bg-white" >
+                        <Col className="p-2" md={6} sm={12} lg={6} >
+                            <img className='w-100' src={imagePlaceholder} />
+                            <button className="btn btn-primary">Capture</button>
+                        </Col>
+                        <Col className="p-2" md={6} sm={12} lg={6} >
+                            <img className='w-100' src={imagePlaceholder} />
+                            <button className="btn btn-primary">Save</button>
+                        </Col>
+                    </Row>
+                </Container>
+            </Fragment>
         );
     }
 }
